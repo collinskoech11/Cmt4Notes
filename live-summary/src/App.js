fd31df18a8af6f89f4dd7656335bb9a0c524958b
collinskoech11 <collinskoechck34@gmail.com>
2021-01-22 22:53:16 +0300

    update

--- live-summary/src/App.js
+++ live-summary/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from './pages/Home';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
-import Navpage from './components/Navpage'
+import Navpage from './components/Navpage';
+import DemoCarousel from './components/DemoCarousel'
 
 function App() {
   return (
     <div className="App">
       <Navpage/>
+      <DemoCarousel/>
       <Router>
        <Route exact path="/" component={Home}/>
       </Router>
--- live-summary/src/components/DemoCarousel.js
+++ live-summary/src/components/DemoCarousel.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import './style.css'
 
 class DemoCarousel extends Component {
     render() {
         return (
             <Carousel>
-                <div>
-                    <img src="https://source.unsplash.com/400x200/?Bishop" />
+                <div className="contain">
+                    <img src="https://source.unsplash.com/1000x200/?pen" alt="Branch head profile"/>
                     <p className="legend">Legend 1</p>
                 </div>
-                <div>
-                    <img src="https://source.unsplash.com/400x200/?Bishop" />
+                <div className="contain">
+                    <img src="https://source.unsplash.com/1000x200/?Computer" alt="Branch head profile" />
                     <p className="legend">Legend 2</p>
                 </div>
-                <div>
-                    <img src="https://source.unsplash.com/400x200/?Bishop" />
+                <div className="contain">
+                    <img src="https://source.unsplash.com/1000x200/?Macbook" alt="Branch head profile" />
                     <p className="legend">Legend 3</p>
                 </div>
             </Carousel>
         );
     }
-});
+};
 
-Rea
+export default DemoCarousel
