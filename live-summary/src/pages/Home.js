50697b74adbb3e2c7f8d25d0209d8cb8ee619ab0
collinskoech11 <collinskoechck34@gmail.com>
2021-01-22 21:17:30 +0300

    update

--- live-summary/src/pages/Home.js
+++ live-summary/src/pages/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
 
 function Home() {
     return (
         <div>
-            
+            <div className="row">
+                <div className="unit">
+                    
+                </div>
+            </div>
         </div>
     )
 }
