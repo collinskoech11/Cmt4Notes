7d74ea6a46fe754f3f65fb3bace4ae1ba2576db6
collinskoech11 <collinskoechck34@gmail.com>
2021-01-25 11:40:56 +0300

    update

--- live-summary/src/App.js
+++ live-summary/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './pages/Home';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import Navpage from './components/Navpage';
+import FooterPage from './components/FooterPage';
 import DemoCarousel from './components/DemoCarousel'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Router>
        <Route exact path="/" component={Home}/>
       </Router>
+      <FooterPage/>
     </div>
   );
 }
--- live-summary/src/components/Navpage.js
+++ live-summary/src/components/Navpage.js
@@ -5,12 +5,12 @@ function Navpage() {
     return (
     <>
         <Navbar bg="blue" expand="lg">
-            <Navbar.Brand href="#home">Live Summary</Navbar.Brand>
+            <Navbar.Brand href="/">Live Summary</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Courses</Nav.Link>
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/System">Courses</Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">Request push access</NavDropdown.Item>
