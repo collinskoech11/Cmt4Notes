2cebbfeb73792a74a2b7ef8d73b2b900c2d00e47
collinskoech11 <collinskoechck34@gmail.com>
2021-02-01 10:39:10 +0300

    content update

---
+++ live-summary/src/pages/CsUnits/Cmt447.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Cmt447() {
+    return (
+        <div className="container">
+            <h1>CMT447:Web Application Security</h1>
+            <br/>
+            <p>
+             -> Information Security <br/>
+             -> Physical Security <br/>
+             -> Database Security <br/>
+             ->Application Security <br/>
+             -> Server Security <br/>
+            </p>
+        </div>
+    )
+}
+
+export default Cmt447
