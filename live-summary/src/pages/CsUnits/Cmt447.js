2274336815bc8110361e6157ef36b62031d9ba74
collinskoech11 <collinskoechck34@gmail.com>
2021-02-02 12:03:26 +0300

    debug update

--- live-summary/src/pages/CsUnits/Cmt447.js
+++ live-summary/src/pages/CsUnits/Cmt447.js
@@ -10,58 +10,58 @@ function Cmt447() {
            
                                                                         <h1> CHAPTER ONE /Lesson(2)     </h1>                                  20/01/2021/LESSON 
             ------------------------------------------------------------------------------------------------------------------------------------------
-            -what is risk in computer security? - anything that can can cause damage to confidentiality, availability and intergrity of data and systems.<br>
-                                                - they include misconfigured software, unpatches operating system, unsafe computer habits.<br>
-            -privileges -<br>
-            -encryption - converting plain text to cipher text.<br>
-            -cipher - plain text that has undergone the cryptographic algorithms, to be much longer than the actual word or being shorter than the word.<br>
-            -decryption – converting the cipher text to plain text.( algorithm and key)<br>
-            -vulnerability -  loop hole in a system that can be exploited.       <br>                                                                                                20/01/2021/LESSON 2
+            -what is risk in computer security? - anything that can can cause damage to confidentiality, availability and intergrity of data and systems.<br/>
+                                                - they include misconfigured software, unpatches operating system, unsafe computer habits.<br/>
+            -privileges -<br/>
+            -encryption - converting plain text to cipher text.<br/>
+            -cipher - plain text that has undergone the cryptographic algorithms, to be much longer than the actual word or being shorter than the word.<br/>
+            -decryption – converting the cipher text to plain text.( algorithm and key)<br/>
+            -vulnerability -  loop hole in a system that can be exploited.       <br/>                                                                                             20/01/2021/LESSON 2
            
-            -what is<br>
-            -integrity - the act of data being protected from being modified, deleted and duplicated by unauthorized person.<br>
-            -availability - the act of systems/ data being available or reliable to the authorized person.<br>
-            -confidentiality - the act of keeping it private or secret<br>
-            -Non- repudiation - associating action to unique individual.<br>
-            -authorization - giving access to authorized individuals.<br>
-            -access control - <br>
-            -spyware - it is a malware that can still your data, and use it to spy on you using webcam or microphone without your concern.<br>
-            -ransome - melicious code that can hold your system or data unreable until you pay the hacker ransom.<br>
-                    -can be delivered by virus or worm.<br>
-            -trojan horse - a special kind of virus.<br>
-                        - delivers other malware by hidding in lagitimate programs.<br>
-            -worm - they are like virus.<br>
-                - they have ability to spread themselves from computer to computer on their own.<br>
-            -virus - malicious code that is attached to a files in your system.<br>
-                -can cause damages,  data lose or cause someone to access your system without your concerns.<br>
-                -SQL injection<br>
-                -cross scripting <br>
-                -Trojan horse<br>
-                - DOS<br>
-            -zombie<br>
-            -botnet<br>
-            -Attack -<br>
-            -Detection<br>
-            -threat – potential acts that can cause a system to be exploited.<br>
-                        - it can cause denial of access to a system.<br>
-            -policy – protective measures that put in place by an organization.<br>
-            -Control – <br>
-            Cybercrime – act of doing illegal activities in the cyber space.<br>
-            Cyberspace –  widespread interconnected digital technology.<br>
-            Penetration testing – <br>
-            Who is a hacker -  an individual who uses the network, computer and other technical skills to solve  <br>  
-                                                the computer/system problems.  <br>                                                              
-            -Type of hackers – <br>
-                                    -Black hat hacker - <br>
-                                    -Grey hat hackers<br>
-                                    -Script kiddies / amateurs<br>
-                                -White hat hackers.<br>
+            -what is<br/>
+            -integrity - the act of data being protected from being modified, deleted and duplicated by unauthorized person.<br/>
+            -availability - the act of systems/ data being available or reliable to the authorized person.<br/>
+            -confidentiality - the act of keeping it private or secret<br/>
+            -Non- repudiation - associating action to unique individual.<br/>
+            -authorization - giving access to authorized individuals.<br/>
+            -access control -<br/>
+            -spyware - it is a malware that can still your data, and use it to spy on you using webcam or microphone without your concern.<br/>
+            -ransome - melicious code that can hold your system or data unreable until you pay the hacker ransom.<br/>
+                    -can be delivered by virus or worm.<br/>
+            -trojan horse - a special kind of virus.<br/>
+                        - delivers other malware by hidding in lagitimate programs.<br/>
+            -worm - they are like virus.<br/>
+                - they have ability to spread themselves from computer to computer on their own.<br/>
+            -virus - malicious code that is attached to a files in your system.<br/>
+                -can cause damages,  data lose or cause someone to access your system without your concerns.<br/>
+                -SQL injection<br/>
+                -cross scripting <br/>
+                -Trojan horse<br/>
+                - DOS<br/>
+            -zombie<br/>
+            -botnet<br/>
+            -Attack -<br/>
+            -Detection<br/>
+            -threat – potential acts that can cause a system to be exploited.<br/>
+                        - it can cause denial of access to a system.<br/>
+            -policy – protective measures that put in place by an organization.<br/>
+            -Control – <br/>
+            Cybercrime – act of doing illegal activities in the cyber space.<br/>
+            Cyberspace –  widespread interconnected digital technology.<br/>
+            Penetration testing – <br/>
+            Who is a hacker -  an individual who uses the network, computer and other technical skills to solve  <br/>
+                                                the computer/system problems. <br/>                                                          
+            -Type of hackers – <br/>
+                                    -Black hat hacker - <br/>
+                                    -Grey hat hackers<br/>
+                                    -Script kiddies / amateurs<br/>
+                                -White hat hackers.<br/>
                                 
-            Technolofies to impliment encryption - DH differ-Hellman)<br>
-                                                    PGP prettty good privacy)<br>
-                                                    -> Information Security <br/>
+            Technolofies to impliment encryption - DH differ-Hellman)<br/>
+                                                    PGP prettty good privacy)<br/>
+                                                    -> Information Security<br/>
              -> Physical Security <br/>
-             -> Database Security <br/>
+             -> Database Security<br/>
              ->Application Security <br/>
              -> Server Security <br/> </p>
         </div>
