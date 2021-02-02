ffae6a88c06ba6135eae3abed8302d4104c82390
collinskoech11 <collinskoechck34@gmail.com>
2021-02-02 12:14:38 +0300

    update

--- live-summary/src/pages/CsUnits/Cmt446.js
+++ live-summary/src/pages/CsUnits/Cmt446.js
@@ -3,7 +3,229 @@ import React from 'react'
 function Cmt446() {
     return (
         <div>
+        <p>
+                      <h1>  CMT 446  page five</h1>
+            -----------
+            Introduction To Information Systen Audit
+            ------------------------------------------
+            -Book - Certified information system Auditor()<br/>
+            -Auditor - Review the strength of the information system control and report their capability to protect the organisation's assets.<br/>
+                    -Responsible for leading, plannig  and performing the audit.<br/>
+                    -Must understand  auditing standards, how to perform an audit, and how to measure risk.<br/>
+                    -Plan specific audit to ensure that IT and businness system are protected and controlled.<br/>
+                    -Evaluate the design and  operation of internal control.<br/>
+                    
+                    Step by step general process in evalaluating the desing and operation of internal control<br/>
+                    -------------------------------------------------------------------------------------------
+            -Verifying compliance with external  regulations<br/>
+                                    -identify the external requirement with which the company is responsible for being in compliance.<br/>
+                                    -Review specific laws and regulations with which the organisation must be compliance.<br/>
+                                    -Determine whether the organisation  considered these laws and regulations when policies and procedure were developed.<br/>
+                                    -Determine whether identified policies and procedures adheres to external laws and requirements.<br/>
+                                    -Determine whether the employees are adhering to specific policies and procedure or whether discrepancies exit.<br/>
+                                    
+                                    Audit planning <br/>
+                                    -------------------
+                                -The goal of the audit  function is to presesnt an independent  and <br/>
+                                    objective evaluation of the state of the internal control with appropriate  recommendation to mitigate any detected risks if applicable.<br/>
+                                    -At the highest level,  the audit function requires an audit charter to establish the IS  audit function.<br/>
+                                    -The charter  -defines what responsibilities senior management is delegating.<br/>
+                                    -----------
+                    
+                    
+
+            Issues and challenges of ISA
+            ---------------------------------
+            -All organinsation must work within the framework of laws and regulations.<br/>
+            -These rules may dictate how data is processed, handled, stored and destroyed.<br/>
+            -If organisation fals to handleinformation with due care and diligence, the could be subjected to fines, loss of public confidence or even jail time.<br/>
+            -They will be liable if personal data is disclosed to an unauthorized person.<br/>
+            -Safe harbor act - this act prohibits the transfer of personal data to non-auropeam union(Nation) that do not meet  the european standard for privacy protection.<br/>
+
+            Regulation standards includes:<br/>
+            -----------------------------------
+            -U.S Health and portability and accountability act(HIPAA)<br/>
+            -sarbanes-oxley act<br/>
+            -Basel Accord standard II<br/>
+            -U.S Federal information Security management Act(FISMA)<br/>
+            -Committee for sponsoring Organization of the Treadyway commission.<br/>
+            -US supervision control and data acquisition <br/>
+            -U.S Fair and accurate credit transaction act of 2003<br/>
+            -ISA will build the strength <br/>
+            -Auditors -reviews the companie's strength  of the IS control are responsible  for leading, planning and performing the audit.<br/>
+                    -Must understand auditing standard, how to perform audit and how to measure risk.<br/>
+                    -They include:<br/>
+                                -financial auditors<br/>
+                                -Compliance audits<br/>
+                                -Information system auditor<br/>
+                                -Business audit<br/>
+                                
+            -Identifies the external requirement for....<br/>
+            -review the specific laws .....<br/>
+            -Determine whether the organisations....<br/>
+            -Determine whether employees are  complying with ...<br/>
+
+
+            -Audit planning<br/>
+            ------------------
+            -the goal of an audit function is to present an independent  and objective evaluation to the state of internal control.<br/>
+            -the audit function require an audit chart.<br/>
+            -The chart function defines the senior delegations.<br/>
+            -the audit committee is like the committee of few managers.<br/>
+            -the audit committee is only responsible to senior management <br/>
+            -Auditors - should report findings to sinior management<br/>
+            -short term planning also tarctical planning which  looks at issue the organisation is currently facing.<br/>
+
+
+            ISACA<br/>
+            --------
+            -Its standard are developed by the ISACA's board.<br/>
             
+            
+                standard Document <br/>
+                ---------------------
+            - professional Ethics and standard<br/>
+            - performance of audit work<br/>
+                -Use of risk accessment<br/>
+                
+                
+                Guideline Document<br/>
+                ------------------------
+                -using the work of other auditors<br/>
+                -use of computer assistence technique<br/>
+                -outsourcing information system of other organisation<br/>
+                -due professional care<br/>
+                -audit documentation<br/>
+                -audit sampling<br/>
+                -IT governance<br/>
+                -Guided document  on online banking.<br/>
+                
+                
+                Procedure documents<br/>
+                -----------------------
+                -information system risk accessment<br/>
+                -irregularities and business act<br/>
+                
+                ISACA   code of ethics<br/>
+                -------------------------
+                -Ethics goes beyond human requirement.<br/>
+                -they are define values and principles that define human behavior.<br/>
+                -They include:<br/>
+                            -Code of conduct.<br/>
+                            -to support the implimentation and  compliance with appropriate stand....<br/>
+                            -perform their duties with due  delligence and profession care in accordance with ...<br/>
+                            -serve in the interest of law holders in a lawful <br/>
+                            -maintain the privacy of information obtain in the cause of their duties unless required by the authory.<br/>
+                            
+                Risk analysis<br/>
+                ---------------
+                -Model networks and systems are highly complex.<br/>
+                - risk is the pottential for harm.<br/>
+                -Vulnerability - is the weakness in a system or process.<br/>
+                -a threat- can be seen as frequency .<br/>
+                -Include <br/>
+                -audit risk - a risjk that the audit might contain audit error or an error can exist without beung detected<br/>
+            - control risk - this the risk that a material arror that mighr excist and  cannot bw detected<br/>
+                -Business risk - a risk that<br/>
+            - Detection risk<br/>
+                -material risk<br/>
+            -Security risk<br/>
+                -ISACA categories inherent control and detection of type of ....<br/>
+                
+                -high risk - Risk that threaten financial........ <br/>
+                
+                Risk management<br/>
+                -------------------
+                -follows a define process which includes:<br/>
+                        -develop a risk management <br/>
+                        -identify threat<br/>
+                        -peform risk mitigation<br/>
+                        -monitor<br/>
+            
+            -Risk can be hundled/mitigating in 4 way:<br/>
+                        -avoid risk.<br/>
+                        -Reduce risk ie installing a firewall in place. <br/>
+                        -accepting risk - Retaining the potential cost  that is obtained by ...<br/>
+                        -Transfering the risk- the organisation faces the risk on behave of someone ie the insurance.<br/>
+                        -
+                        
+                Risk based audit<br/>
+                ----------------------
+            -Steps followed in a :<br/>
+                    1-gather information and plan<br/>
+                    2 -understand the bussines<br/>
+                    -Review audit<br/>
+                    -Evaluate <br/>
+                    3-Determine the internal control and review functionality<br/>
+                    4-perform compliance test <br/>
+                    -perfom substandard test <br/>
+                    -conclude the audit by preparing a report <br/>
+                
+                
+                
+                
+                
+                Auditing of internal control <br/>
+                ----------------------------------
+                -conterol tipocal start with high level policy and applies in all area in company <br/>
+                -information system - verifies that the information system are mentained in a controled state <br/>
+                -information system procedures -procedures that provide reasonable assurance for the control of database system. <br/>
+                -informatipn sysem auditors - should understand how each general proceudre co-relate. <br/>
+                
+                -Control objective for information and related technologies - a frameworejk designed across 4 domain <br/>
+                -Main domains includes: <br/>
+                        -plan and organise <br/>
+                        -acquire and implement <br/>
+                        -delive and support <br/>
+                        -monitor and evaluate <br/>
+            
+            
+            Audit process  <br/>
+            ----------------
+            -An audit can be defined an a plan, independent and document accessment to determine whether agree upon required and standard of operation have been made. <br/>
+            -An information system audit is similar to the other audit  ie finanncial accept it deals with computerised system that hundles information. <br/>
+            -this system can be fully computerised or still be partially audited <br/>
+            -An auditor should build confident and trust.
+            
+            
+            Audit classification <br/>
+            ------------------------
+            -Audit can either be internal or external, they include: <br/>
+                        -Administrative audit <br/>
+                        -financial audit <br/>
+                        -Forensic audit - focus on information that ulter... <br/>
+                        -operational audit <br/>
+                        
+                        
+                    
+            -Audit methodology <br/>
+            -------------------------
+            -A documentation approach for performing ...... <br/>
+            -Designed to meet........ <br/>
+                    define the: <br/>
+                    .statement of work <br/>
+                    .statement of scope objective <br/>
+                        
+            -   
+            audit process <br/>
+            ---------------
+            -
+            -
+
+            Audit sampling  <br/>
+            ---------------
+            -They two type of audit sampling. <br/>
+            -statistical sample is based on probability. <br/>
+                        can measure risk and the use of ..... <br/>
+            -nonstatistical sampling  -the auditor uses his own judgement, also called judgemental sampling. <br/>
+            -Variable sampleing <br/>
+            -stop and go sampling  <br/>
+            -frequency and .......... <br/>
+                
+                
+            -evidence - is information that an auditor obtain to assist. <br/>
+                    - should be sufficient, reliable, usable relavant and effective. <br/>
+                    </p>
         </div>
     )
 }
