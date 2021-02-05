2f244b0a1f1cd1edd957d339bd62093a66437762
collinskoech11 <collinskoechck34@gmail.com>
2021-02-05 10:25:15 +0300

    update

--- live-summary/src/components/Popup.js
+++ live-summary/src/components/Popup.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';  
+import './style.css';  
 
-function Popup() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+class Popup extends React.Component {  
+  render() {  
+return (  
+<div className='popup'>  
+    <div className='popup\_inner'>  
+        <h1>{this.props.text}</h1>  
+        <button onClick={this.props.closePopup}>close me</button>  
+    </div>  
+</div>  
+);  
+}  
+}  
 
-export default Popup
+export default Popup;
