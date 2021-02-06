9d882f3d8ad45709b1e3d65bfafb599388bda8c3
collinskoech11 <collinskoechck34@gmail.com>
2021-02-06 19:21:03 +0300

    update

--- live-summary/src/Container.js
+++ live-summary/src/Container.js
@@ -4,12 +4,26 @@ import TriggerButton from '../TriggerButton';
 export class Container extends Component {
 state = { isShown: false };
 render() {
-   return (
-    <TriggerButton
-    triggerText={this.props.triggerText}
-    />
-{this.state.isShown ? (<Modal/>) : null}
-);
+return (
+<React.Fragment>
+<TriggerButton
+  showModal={this.showModal}
+  buttonRef={(n) => (this.TriggerButton = n)}
+  triggerText={this.props.triggerText}
+/>
+{this.state.isShown ? (
+  <Modal
+    onSubmit={this.props.onSubmit}
+    modalRef={(n) => (this.modal = n)}
+    buttonRef={(n) => (this.closeButton = n)} 
+    closeModal={this.closeModal}
+    onKeyDown={this.onKeyDown}
+    onClickOutside={this.onClickOutside}
+  />
+  ) : null
+ }
+</React.Fragment>
+  );
  }
 }
 export default Container;
