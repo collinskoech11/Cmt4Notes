addf7b2d370dc8af37c6978686f21cdaea084453
collinskoech11 <collinskoechck34@gmail.com>
2021-02-12 20:59:21 +0300

    update

---
+++ live-summary/src/pages/Upcoming.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Collapsible from 'react-collapsible';
+//One Punch man
+
+function Upcoming() {
+    return (
+       <Collapsible trigger="Ict and Society">
+            <p>
+                This is the collapsible content. It can be any element or React
+                component you like.
+            </p>
+            <p>
+                It can even be another Collapsible component. Check out the next
+                section!
+            </p>
+       </Collapsible>
+    )
+}
+
+export default Upcoming
