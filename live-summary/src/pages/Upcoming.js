86668d5da40dc96190d735fd5bd82a5218e2b5ae
collinskoech11 <collinskoechck34@gmail.com>
2021-02-16 08:22:09 +0300

    Update Upcoming.js

--- live-summary/src/pages/Upcoming.js
+++ live-summary/src/pages/Upcoming.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Collapsible from 'react-collapsible';
+import './style.css'
 //One Punch man
 
 function Upcoming() {
     return (
-       <Collapsible  trigger="Ict and Society">
+        <>
+       <Collapsible  trigger="ICT AND SOCIETY">
             <h3>****Due 22 Feb 2021</h3>
             1) Dsicusss similarities between ethics and law (4mks)<br/>
                 -Discuss the difference between ethics and law (5mks)<br/>
@@ -12,8 +14,17 @@ function Upcoming() {
             3) Describe Cyber crimes(3)<br/>
             Intellectual property(2mks)<br/>
             Cyber Crime (2mks)<br/>
-            
        </Collapsible>
+
+        <Collapsible  trigger="NETWORK AND SYSTEM ADMINISTRATION">
+            <strong>For a successsful project schedule we strat with understanding the corporate structure which helps us to understand the data flow in the organisation, Develop a network topology which is parallel to corporate structure This eventually helps managers(project managers who will have the authoritiy to accept or reject the network deign ) This conflict is essential to allow us to come up with a proper network design</strong><br/>
+                ** REv qn: <br/> 
+                1) Why is it important to use a structured, Systematic method for designing networks<br/>
+                2) What problems could occur if such methods are not used<br/>
+                3) Why is it important to explore divisional and group structure of an organisation when starting a network design project<br/>
+
+        </Collapsible>
+        </>
     )
 }
 
