528c4a0aba9b51dd416b7ad16cb40c94bc77917c
collinskoech11 <collinskoechck34@gmail.com>
2021-02-16 22:32:49 +0300

    update

--- live-summary/src/pages/ReqAccess.js
+++ live-summary/src/pages/ReqAccess.js
@@ -19,11 +19,11 @@ export default function  ReqAccess(){
         <div>
            <form className="contact-form" onSubmit={sendEmail}>
                 <input type="hidden" name="contact_number" />
-                <label>Name</label>
+                <label>Name :</label>
                 <input type="text" name="user_name" />
-                <label>Email</label>
+                <label>Email :</label>
                 <input type="email" name="user_email" />
-                <label>Message</label>
+                <label>Message :</label>
                 <textarea name="message" />
                 <input type="submit" value="Send" />
                 </form> 
