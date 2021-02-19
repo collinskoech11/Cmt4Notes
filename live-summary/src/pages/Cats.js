c853624342fc0145d0a7787afdb89e512c6b1320
collinskoech11 <collinskoechck34@gmail.com>
2021-02-19 12:54:20 +0300

    update

--- live-summary/src/pages/Cats.js
+++ live-summary/src/pages/Cats.js
@@ -3,37 +3,38 @@ import './style.css';
 import Emoji from "react-emoji-render";
 import Collapsible from 'react-collapsible';
 
+
 function Cats() {
     return (
         <>
             <Collapsible  trigger="ICT AND SOCIETY">
                 <h3>Date not set</h3>
-                <p>Its best to keep studying <Emoji /> </p>    
+                <p>Its best to keep studying <Emoji text=" :+1:  :)" /> </p>    
             </Collapsible>
 
             <Collapsible  trigger="NETWORK AND SYSTEM ADMINISTRATION">
                 <h3>Date not set</h3>
-                <p>Its best to keep studying <Emoji /> </p> 
+                <p>Its best to keep studying <Emoji text=" :+1:  :)" /> </p>  
             </Collapsible>
 
             <Collapsible  trigger="SOFTWARE PROJECT MANAGEMENT">
-            <h3>Date not set</h3>
-                <p>Its best to keep studying <Emoji /> </p> 
+                <h3>Date not set</h3>
+                <p>Its best to keep studying <Emoji text=" :+1:  :)" /> </p>  
             </Collapsible>
 
             <Collapsible  trigger="ADVANCED ARTIFICIAL INTELLIGENCE">
-            <h3>Date not set</h3>
-                <p>Its best to keep studying <Emoji /> </p> 
+                <h3>Date not set</h3>
+                <p>Its best to keep studying <Emoji text=" :+1:  :)" /> </p>  
             </Collapsible>
 
             <Collapsible  trigger="WEB APPLICATION SECURITY">
-            <h3>Date not set</h3>
-                <p>Its best to keep studying <Emoji /> </p> 
+                <h3>Date not set</h3>
+                <p>Its best to keep studying <Emoji text=" :+1:  :)" /> </p>  
             </Collapsible>
 
             <Collapsible  trigger="SYSTEM AUDIT">
-            <h3>Date not set</h3>
-                <p>Its best to keep studying <Emoji /> </p> 
+                <h3>Date not set</h3>
+                <p>Its best to keep studying <Emoji text=" :+1:  :)" /> </p>  
             </Collapsible>
         </>
     )
