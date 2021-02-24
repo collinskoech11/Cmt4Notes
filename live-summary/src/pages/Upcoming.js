b96b5d9cc193b2d0a6550d57cc29325834daa2a9
collinskoech11 <collinskoechck34@gmail.com>
2021-02-25 00:51:02 +0300

    content update

--- live-summary/src/pages/Upcoming.js
+++ live-summary/src/pages/Upcoming.js
@@ -7,13 +7,7 @@ function Upcoming() {
     return (
         <>
        <Collapsible  trigger="ICT AND SOCIETY">
-            <h3>****Due Today</h3>
-            1) Dsicusss similarities between ethics and law (4mks)<br/>
-                -Discuss the difference between ethics and law (5mks)<br/>
-            2) REfer to the computer misyuse and cyber crime act 2018 and Intellectual property act and briefflly discuss their opinion on intellectual property <br/>
-            3) Describe Cyber crimes(3)<br/>
-            Intellectual property(2mks)<br/>
-            Cyber Crime (2mks)<br/>
+             <iframe title="iframe" src='https://view.officeapps.live.com/op/embed.aspx?src=http://remote.url.tld/path/to/document.doc' width='100%' height='300px' frameborder='0'>This is an embedded <a  href='http://office.com'>Microsoft Office</a> document, powered by <a  href='http://office.com/webapps'>Office Online</a>.</iframe>
        </Collapsible>
 
         <Collapsible  trigger="NETWORK AND SYSTEM ADMINISTRATION">
