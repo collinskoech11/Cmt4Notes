f62e3559f8cd7a873d3c6c25ceed4aa83273d202
collinskoech11 <collinskoechck34@gmail.com>
2021-02-28 17:10:35 +0300

    update

--- live-summary/src/pages/Cats.js
+++ live-summary/src/pages/Cats.js
@@ -8,12 +8,12 @@ function Cats() {
     return (
         <>
             <Collapsible  trigger="ICT AND SOCIETY">
-                <h3>1st March</h3>
-                <p>Physical Cat <Emoji text=" :+1:  :)" /> </p>    
+                <h3>15th March</h3>
+                <p>Physical Cat <Emoji text=" :(" /> </p>    
             </Collapsible>
 
             <Collapsible  trigger="NETWORK AND SYSTEM ADMINISTRATION">
-                <h3>Date not set</h3>
+                <h3>5th March</h3>
                 <p>Its best to keep studying <Emoji text=" :+1:  :)" /> </p>  
             </Collapsible>
 
@@ -28,7 +28,7 @@ function Cats() {
             </Collapsible>
 
             <Collapsible  trigger="WEB APPLICATION SECURITY">
-                <h3>Date not set</h3>
+                <h3>3rd March</h3>
                 <p>Its best to keep studying <Emoji text=" :+1:  :)" /> </p>  
             </Collapsible>
 
