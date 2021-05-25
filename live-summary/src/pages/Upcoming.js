0112585977556173127a49f661ec9348e852c0c3
collinskoech11 <collinskoechck34@gmail.com>
2021-05-25 07:51:02 +0300

    upcoming update

--- live-summary/src/pages/Upcoming.js
+++ live-summary/src/pages/Upcoming.js
@@ -7,21 +7,15 @@ function Upcoming() {
     return (
         <>
        <Collapsible  trigger="ICT AND SOCIETY">
-             <iframe title="iframe" src='https://view.officeapps.live.com/op/embed.aspx?src=http://remote.url.tld/path/to/document.doc' width='100%' height='300px' frameborder='0'>This is an embedded <a  href='http://office.com'>Microsoft Office</a> document, powered by <a  href='http://office.com/webapps'>Office Online</a>.</iframe>
+                <iframe title="iframe" src='https://view.officeapps.live.com/op/embed.aspx?src=http://remote.url.tld/path/to/document.doc' width='100%' height='300px' frameborder='0'>This is an embedded <a  href='http://office.com'>Microsoft Office</a> document, powered by <a  href='http://office.com/webapps'>Office Online</a>.</iframe>
        </Collapsible>
 
         <Collapsible  trigger="NETWORK AND SYSTEM ADMINISTRATION">
-                ** REv qn: <br/> 
-                1) Why is it important to use a structured, Systematic method for designing networks<br/>
-                2) What problems could occur if such methods are not used<br/>
-                3) Why is it important to explore divisional and group structure of an organisation when starting a network design project<br/>
-                4) Which device stops broadcast trafficking <br/>
-
+                <iframe title="iframe" src='https://view.officeapps.live.com/op/embed.aspx?src=http://remote.url.tld/path/to/document.doc' width='100%' height='300px' frameborder='0'>This is an embedded <a  href='http://office.com'>Microsoft Office</a> document, powered by <a  href='http://office.com/webapps'>Office Online</a>.</iframe>
         </Collapsible>
 
         <Collapsible  trigger="SOFTWARE PROJECT MANAGEMENT">
-        <h5> What are the roles of the following in a project:<br/>
-               Top Management <br/> Project Board <br/> Project Management <br/>  Project Team</h5>
+                <iframe title="iframe" src='https://view.officeapps.live.com/op/embed.aspx?src=http://remote.url.tld/path/to/document.doc' width='100%' height='300px' frameborder='0'>This is an embedded <a  href='http://office.com'>Microsoft Office</a> document, powered by <a  href='http://office.com/webapps'>Office Online</a>.</iframe>
         </Collapsible>
 
         <Collapsible  trigger="ADVANCED ARTIFICIAL INTELLIGENCE">
